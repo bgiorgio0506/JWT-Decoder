e1f705b847414379f66662def4dc4a592a4d971d
bgiorgio0506 <bgiorgio0506@gmail.com>
2019-05-28 16:51:02 +0300

    Fixed problem application not founding css files for two windows

--- application/application_js/decode.js
+++ application/application_js/decode.js
@@ -1,3 +1,10 @@
 //bring in electron and jsonwebtoken
-const {app} = require('electron')
+const {app,BrowserWindow,ipcMain} = require('electron')
 const jwt = require('jsonwebtoken')
+
+//listening for the event
+// receive message from decode.html
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log( arg )
+  event.sender.send('asynchronous-reply',arg)
+  })
