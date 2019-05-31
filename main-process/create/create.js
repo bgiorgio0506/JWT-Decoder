eea6e83828eb353e001a9c4a6dd5843c24738635
bgiorgio0506 <bgiorgio0506@gmail.com>
2019-05-31 23:53:49 +0300

    v 0.5.0

--- main-process/create/create.js
+++ main-process/create/create.js
@@ -1,19 +1,33 @@
 const {ipcMain,dialog} = require('electron');
-const jwt = require('jsonwebtoken');
+const jsonwebtoken = require('jsonwebtoken');
 
 //listening for a message event
-ipcMain.on('asynchronous-message', (event, key, payload, options, exp, iss) =>{
-  if(options === null){
-    let jsontoken = jwt.sign({exp:exp,
-      iss:iss,
-      data:payload
-    },key)
-  }else if (exp !== null && iss !== null) {
-    let jsontoken = jwt.sign({
-      exp:exp,
-      iss:iss,
-      data:payload
-    },key,options)
+ipcMain.on('asynchronous-message', (event, jwt) =>{
+
+  if(jwt.algorithm == null){
+   jsonwebtoken.sign({
+      exp:jwt.exp,
+      iss:jwt.iss,
+      aud:jwt.aud,
+      data:jwt.payload
+    },jwt.key,function (err, token) {
+      console.log(err);
+      console.log(token);
+    })
+  }else {
+    jsonwebtoken.sign({
+      exp:jwt.exp,
+      iss:jwt.iss,
+      aud:jwt.aud,
+      data:jwt.payload
+    },jwt.key,jwt.algo,function(err,token) {
+      if (err){
+        console.log(err)
+        event.sender.send('error-generic','Unexpected Error While Generating Token')
+      }
+      console.log(token);
+    })
   }
-  console.log(jsontoken)
+
+  console.log(token)
 })
--- main-process/error.js
+++ main-process/error.js
@@ -5,3 +5,17 @@ ipcMain.on('error-generic', (event, arg) =>{
   console.log(`Error: ${arg}`)
   dialog.showErrorBox('An Error Accured',arg)
 })
+
+//info Handling
+ipcMain.on('info-generic', (event, arg) =>{
+  console.log(`Info: ${arg}`)
+  let message ={
+    type: 'info',
+    title:'Information',
+    message: arg,
+    buttons:['Yes', 'No']
+  }
+  dialog.showMessageBox(message,(index)=>{
+    event.sender.send('info-answer',index)
+  })
+})
